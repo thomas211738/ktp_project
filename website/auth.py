4ddd35e8ad3b7d39ad93ee0376127fcbbb0ec818
thomas211738 <thomas211738@gmail.com>
2023-11-09 00:22:06 -0500

    c

--- website/auth.py
+++ website/auth.py
@@ -3,6 +3,10 @@ from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
+@auth.route('/home')
+def home():
+    return render_template("home.html")
+
 @auth.route('/login', methods=['GET','POST'])
 def login():
     return render_template("login.html")
--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
 
-from flask import Blueprint, render_template
+# from flask import Blueprint, render_template
 
-views = Blueprint('views', __name__)
+# views = Blueprint('views', __name__)
 
-@views.route('/')
-def home():
-    return render_template("home.html")
+# @views.route('/home')
+# def home():
+#     return render_template("home.html")
