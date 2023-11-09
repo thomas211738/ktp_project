d178bffeb3bbdb35b61c9fe8785d50e7f1cf5a8e
thomas211738 <thomas211738@gmail.com>
2023-11-09 04:06:00 -0500

    sign-up

--- website/auth.py
+++ website/auth.py
@@ -89,18 +89,23 @@ def sign_up():
     
     if request.method == 'POST':
         email = request.form.get('email')
-        first_name = request.form.get('firstName')
+        first_name = request.form.get('firstname')
+        last_name = request.form.get('lastname')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
-            flash('Email must be greater than 3 characters.', category='error')
-        elif len(first_name) < 2:
+
+        if len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error')
-        elif password1 != password2:
-            flash('Passwords don\'t match.', category='error')
+        elif len(first_name) < 2:
+            flash('Last name must be greater than 1 character.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+
         else:
             flash('Account successfully created!2', category='success')
             
