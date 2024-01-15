6892a6874cec57fa59c60b3510e35d63ed4759e8
Thomas Boles <thomas211738@gmail.com>
2024-01-15 00:39:09 -0500

    c

--- Backend/config.js
+++ Backend/config.js
@@ -1,2 +1,2 @@
 export const PORT = 3000; // ideally save port in .env file
-export const mongoDBURL = "mongodb+srv://pharaoh:<Samoht211738$>@ktpledge.8hqaocq.mongodb.net/";
+export const mongoDBURL = "mongodb+srv://pharaoh:Samoht211738$@ktpledge.8hqaocq.mongodb.net/";
